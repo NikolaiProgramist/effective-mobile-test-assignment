6fe95f31508a5ddf1d35016c720151eca879b80d
NikolaiProgramist <nikluk2005@mail.ru>
2026-01-23 20:38:01 +0300

    Add truncate() methods for User and Task models to the DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::truncate();
+        Task::truncate();
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
